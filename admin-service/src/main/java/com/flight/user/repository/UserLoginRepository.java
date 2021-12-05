4e91a38e55692e47d2bff3374649a2337d35ae77
abhinav <abhinavrjirapure123.com>
2021-12-05 16:24:31 +0530

    Flight Booking Admin Service First Commit

---
+++ admin-service/src/main/java/com/flight/user/repository/UserLoginRepository.java
@@ -0,0 +1,15 @@
+package com.flight.user.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.flight.user.entity.UserLoginEntity;
+
+
+@Repository
+public interface UserLoginRepository extends JpaRepository<UserLoginEntity,Long>{
+
+	/*@Query(value="Select u.user_Name,u.password from user_details u where u.userId = :userId",nativeQuery=true)
+	List<UserLoginEntity> findByUserID(@Param("userId") long userId);*/
+}
